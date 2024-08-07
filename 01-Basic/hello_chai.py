3f7a305edb9be74fffcda6ceb7d8ce3e5c92ee2c
shahing <arrestshaheen@gmail.com>
2024-08-07 21:24:23 +0100

    basic

---
+++ 01-Basic/hello_chai.py
@@ -0,0 +1,12 @@
+print("Chai and python")
+
+def chai(n):
+    print(n)
+    
+chai(4)
+chai("lemon tea")
+
+    
+    
+    
+    
--- Tutorial/2.py
+++
@@ -1,7 +0,0 @@
-str = "shahin"
-l = len(str)
-
-for i in range(l):
-    print(str.count(str[i]))
-
-    for j in range(l)
